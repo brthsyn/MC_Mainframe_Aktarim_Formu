b42b1608a78b4cd612865832f10e441806222677
brthsyn <berathuseyincicek@hotmail.com>
2018-07-13 14:14:06 +0300

    added a dummy button

--- Aktarım_Formu/Form1.Designer.cs
+++ Aktarım_Formu/Form1.Designer.cs
@@ -54,6 +54,7 @@
             this.label6 = new System.Windows.Forms.Label();
             this.memberNameTb = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.NewcopyGroup.SuspendLayout();
@@ -105,6 +106,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.nameOfEngineerTb);
             this.groupBox1.Controls.Add(this.timeofchange);
             this.groupBox1.Controls.Add(this.label8);
@@ -413,6 +415,15 @@
             this.label5.TabIndex = 0;
             this.label5.Text = "Member Name :";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(505, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -464,6 +475,7 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.ComboBox timeofchange;
         private System.Windows.Forms.TextBox nameOfEngineerTb;
+        private System.Windows.Forms.Button button1;
     }
 }
 
